206eb3be9559dec10e8e4e91dbb546bdc5047e0b
USER <sithiraroneth@gmail.com>
2025-01-30 22:12:00 +0530

    add mvc architecture

--- util/AppUtil.ts
+++ util/AppUtil.ts
@@ -1,5 +1,5 @@
 import {Request} from "express";
-import fileUpload, {UploadedFile} from "express-fileupload";
+import {UploadedFile} from "express-fileupload";
 export function extractImages(req:Request){
     const image = req.files?.image as UploadedFile;
     return image?image.data:Buffer.alloc(0);
