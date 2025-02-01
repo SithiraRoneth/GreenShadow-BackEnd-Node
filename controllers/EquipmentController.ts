bc9efe664db6c2e2f522b3ae5c76a2ab96f66537
USER <sithiraroneth@gmail.com>
2025-02-01 21:54:07 +0530

    edit equipment

--- controllers/EquipmentController.ts
+++ controllers/EquipmentController.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import {AddEquip} from "../service/EquipmentService";
+import {AddEquip, DeleteEquip, GetAllEquip, UpdateEquip} from "../service/EquipmentService";
 
 const router = express.Router();
 
@@ -12,6 +12,38 @@ router.post('/addEquip',async (req,res)=>{
         res.status(200).send(addedEquip);
     }catch (err){
         console.log("Error during equipment adding :", err)
+    }
+});
+
+router.put('/updateEquip:equipCode', async (req,res)=>{
+    const equipCode = req.params.equipCode;
+    const equip = req.body;
+
+    try{
+        const updatedEquip = await UpdateEquip(equipCode,equip);
+        res.status(201).json({message : "Equipment Updated" , equip:equip})
+    }catch (err){
+        console.log("Error during equipment : ", err);
+    }
+});
+
+router.delete('/deleteEquip:equipCode', async (req,res)=>{
+    const equipCode = req.params.equipCode;
+
+    try{
+        const deletedEquip = await DeleteEquip(equipCode);
+        res.status(201).json({message:"Equipment Deleted", deletedEquip});
+    }catch (err){
+        console.log("Error during equipment : ", err);
+    }
+});
+
+router.get('/getEquips', async (req,res)=>{
+    try {
+        const getAllEquip = await GetAllEquip();
+        res.status(201).json(getAllEquip);
+    }catch (err){
+        console.log("Error getting Equipments Details : ", err);
     }
 })
 
--- service/EquipmentService.ts
+++ service/EquipmentService.ts
@@ -1,5 +1,6 @@
 import {Equipment} from "../model/Equipment";
 import {prisma} from "../database/prisma-data-store";
+import e from "express";
 
 export async function AddEquip(e:Equipment){
     try {
@@ -13,5 +14,41 @@ export async function AddEquip(e:Equipment){
         console.log("Equipment Added : " , newEquip);
     }catch (err){
         console.log("Error during equipment adding :", err)
+    }
+}
+
+export async function UpdateEquip(equipCode:string,equip:Equipment){
+    try{
+        const updatedEquip = await prisma.equipment.update({
+            where:{equipmentCode:equipCode},
+            data:{
+                equipmentName:equip.equipmentName,
+                equipmentType:equip.equipmentStatus
+            }
+        })
+        console.log("Equipment were updated : ", updatedEquip);
+        return updatedEquip;
+    }catch (err){
+        console.log("Error during updating equipment : ", err);
+    }
+}
+
+export async function DeleteEquip(equipCode:string){
+    try{
+        await prisma.equipment.delete({
+            where:{equipmentCode:equipCode}
+        })
+        console.log("Equipment Deleted")
+    }catch (err){
+        console.log("Error during equipment deleting : ", err);
+    }
+}
+
+export async function GetAllEquip(){
+    try {
+        const getAll = await prisma.equipment.findMany();
+        return getAll;
+    }catch (err){
+        console.log("Error getting equipment details : ", err);
     }
 }
