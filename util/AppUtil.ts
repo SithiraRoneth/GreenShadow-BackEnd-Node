09b1e60a6642bf73052a3c664085ea9beaefb46b
USER <sithiraroneth@gmail.com>
2025-02-05 13:55:54 +0530

    edit extractImage()

--- util/AppUtil.ts
+++ util/AppUtil.ts
@@ -1,6 +1,7 @@
 import {Request} from "express";
 import {UploadedFile} from "express-fileupload";
-export function extractImages(req:Request){
+
+export function extractImages(req: Request) {
     const image = req.files?.image as UploadedFile;
-    return image?image.data:Buffer.alloc(0);
+    return image ? image.data : Buffer.alloc(0);
 }
